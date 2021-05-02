bad20d8e37bb66dca6bc7320942cbe0d282d155e
Nikhil Ganta <nikhilganta95@gmail.com>
2021-05-02 15:44:54 -0600

    Testing the Build

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def require_jwt(function):
 
 @APP.route('/', methods=['POST', 'GET'])
 def health():
-    return jsonify("Healthy")
+    return jsonify("Healthy Commit")
 
 
 @APP.route('/auth', methods=['POST'])
